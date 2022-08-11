c5266a2c3972b8cff0a43e888015655343b67f07
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-08-11 15:47:44 +0900

    🤡

--- MentorBook/ViewController.swift
+++ MentorBook/ViewController.swift
@@ -8,10 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var courseLabel: UILabel!
+    
+    var mentorArray: [Mentor] = []
+    
+    var index: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mentorArray.append(Mentor(name: "ながた", imageName: "nagata.jpg", course: "iPhone"))
+        mentorArray.append(Mentor(name: "りょう", imageName: "ryo.jpg", course: "Untiy"))
+        mentorArray.append(Mentor(name: "たいてぃ", imageName: "taithi.jpg", course: "webS,webD"))
+        
+        setUI()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func mae(){
+        index = index - 1
+        setUI()
+        
+    }
+    
+    @IBAction func tugi(){
+        index = index + 1
+        setUI()
+        
+    }
+    
+    func setUI(){
+        indexRange()
+        
+        imageView.image = mentorArray[index].getImage()
+        nameLabel.text = mentorArray[index].name
+        courseLabel.text = mentorArray[index].course
+        
+    }
+    
+    func indexRange(){
+        if index >= 3 {
+            index = 0
+        } else if index <= -1 {
+            index = 2
+        }
     }
 
 
